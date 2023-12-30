490e9732a0165420d6a3bc745ca213cde98a0697
Choi-Young-Hoon <hun5077@gmail.com>
2023-12-30 14:48:42 +0900

    리눅스 빌드 수정

--- build_example_command.sh
+++ build_example_command.sh
@@ -2,7 +2,7 @@
 
 mkdir cmake_build
 cd ./cmake_build
-cmake -DCMAKE_BUILD_TYPE=Release -DFFMPEG_INCLUDE_DIR="D:\git\ffmpeg_lib\ffmpeg\include" -DFFMPEG_LIBRARY_DIR="D:\git\ffmpeg_lib\ffmpeg\lib" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DFFMPEG_INCLUDE_DIR="/usr/include/x86_64-linux-gnu/" -DFFMPEG_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu/" ..
 make -j10
 make install
 cd ..
